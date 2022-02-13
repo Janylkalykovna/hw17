c49b0bfd9da24bb2f1255d2aa10c3b4c8032711f
User <janylkalykovna>
2022-02-13 15:30:44 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal[] animals = {new Turtle(), new Eagle(), new Shark()};
+        Animal[] rep = new Turtle[4];
+        for (Animal a : animals) {
+            if (a instanceof Turtle)
+                ((Turtle) a).swim();
+            if (a instanceof Eagle)
+                ((Eagle) a).fly();
+            if (a instanceof Shark) ((Shark) a).attack();
+        }
+
+    }
+}
+
---
+++ src/com/company/Turtle.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Turtle extends Animal{
+    @Override
+    public void eat() {
+        super.eat();
+    }
+
+    public void swim(){
+        System.out.println("Turtle is swimming");
+    }
+}
